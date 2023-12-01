3bb55a8d5e8f0ad24c906e3619b03278cd68a367
william <willy4543@gmail.com>
2023-12-01 14:29:23 +0800

    init project

---
+++ src/main/java/cc/synpulse8/bankcoreservice/BankCoreServiceApplication.java
@@ -0,0 +1,13 @@
+package cc.synpulse8.bankcoreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankCoreServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cc/synpulse8/bankcoreservice/BankCoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.synpulse8.bankcoreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
